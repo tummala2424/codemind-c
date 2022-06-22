c14c05035f3d7264d028bae75f31ff2c764f9d8d
Devika <97424635+tummala2424@users.noreply.github.com>
2022-06-22 10:51:16 +0530

    #include<stdio.h> int main() {     int n,k=0,c=0,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         c=0;         for(j=1;j<=n;j++)         {             if(i*j==n)             {                 if(i-j==1)                 {                     c++;                     k=1;                 }             }         }         if(c==1)         {             break;         }         else         {             continue;         }         break;     }     if(k==1)     {         printf("YES");     }     else     {         printf("NO");     }     return 0; }

---
+++ Pronic_Number_or_Heteromecic_Number.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n,k=0,c=0,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        c=0;
+        for(j=1;j<=n;j++)
+        {
+            if(i*j==n)
+            {
+                if(i-j==1)
+                {
+                    c++;
+                    k=1;
+                }
+            }
+        }
+        if(c==1)
+        {
+            break;
+        }
+        else
+        {
+            continue;
+        }
+        break;
+    }
+    if(k==1)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
