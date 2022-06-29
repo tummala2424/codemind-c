91758a12963bcb4e22a913f998075a060a85025f
Devika <97424635+tummala2424@users.noreply.github.com>
2022-06-29 18:53:12 +0530

    #include<stdio.h> int main() {     int n,i,j,c=0,d=0;     scanf("%d",&n);     for(i=35;i<=n;i++)     {         c=0;         for(j=1;j<=i;j++)         {             if(i%j==0)             {                 c++;             }         }         if(c==9)         {             d++;             printf("%d ",i);         }     }     printf(" ");     printf("Total=%d ",d);     return 0; }

---
+++ Program_to_find_the_number_of_integers_with_exactly_9_divisors.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c=0,d=0;
+    scanf("%d",&n);
+    for(i=35;i<=n;i++)
+    {
+        c=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                c++;
+            }
+        }
+        if(c==9)
+        {
+            d++;
+            printf("%d ",i);
+        }
+    }
+    printf("
+");
+    printf("Total=%d
+",d);
+    return 0;
+}
