82e82fbb6331435756997850fd86073ddd616bee
Devika <97424635+tummala2424@users.noreply.github.com>
2022-06-29 21:29:45 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         s=s+arr[i];     }     printf("%d ",s);     return 0; }

---
+++ Find_the_sum_of_elements_in_the_array_without_using_loops.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+arr[i];
+    }
+    printf("%d
+",s);
+    return 0;
+}
