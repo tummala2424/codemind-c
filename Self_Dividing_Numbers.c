13113808aa5078bc32a455e232ae78689c04b75c
Devika <97424635+tummala2424@users.noreply.github.com>
2022-06-29 18:59:09 +0530

    #include<stdio.h> int main() {     int a,b,i,temp=0,c=0,d=0;     scanf("%d ",&a);     scanf("%d ",&b);     for(i=a;i<=b;i++)     {         temp=i;         while(temp!=0)         {             d=temp%10;         if(d==0 || i%d!=0)         {             break;         }         temp=temp/10;     }     if(temp==0)     {         printf("%d ",i);     }     }     return 0; }

---
+++ Self_Dividing_Numbers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,temp=0,c=0,d=0;
+    scanf("%d
+",&a);
+    scanf("%d
+",&b);
+    for(i=a;i<=b;i++)
+    {
+        temp=i;
+        while(temp!=0)
+        {
+            d=temp%10;
+        if(d==0 || i%d!=0)
+        {
+            break;
+        }
+        temp=temp/10;
+    }
+    if(temp==0)
+    {
+        printf("%d ",i);
+    }
+    }
+    return 0;
+}
