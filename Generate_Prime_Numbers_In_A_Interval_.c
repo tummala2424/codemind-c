a1a7ca070c8378af61c1e614763dae803fea8653
Devika <97424635+tummala2424@users.noreply.github.com>
2022-07-02 14:00:23 +0530

    #include<stdio.h> #include<math.h> int main() {     int m,n,i,j,c=0,d=0;     scanf("%d%d",&n,&m);     if(n==1)     n=2;     for(i=n;i<=m;i++)     {         d=0;         for(j=2;j<=sqrt(i);j++)         {             if(i%j==0)             {                 d++;             }         }         if(d==0)         {             printf("%d ",i);         }     }     return 0; }

--- Generate_Prime_Numbers_In_A_Interval_.c
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -2,21 +2,21 @@
 #include<math.h>
 int main()
 {
-    int m,n,i,j,c=0,c1=0;
+    int m,n,i,j,c=0,d=0;
     scanf("%d%d",&n,&m);
     if(n==1)
     n=2;
     for(i=n;i<=m;i++)
     {
-        c=0;
+        d=0;
         for(j=2;j<=sqrt(i);j++)
         {
             if(i%j==0)
             {
-                c++;
+                d++;
             }
         }
-        if(c==0)
+        if(d==0)
         {
             printf("%d
 ",i);
